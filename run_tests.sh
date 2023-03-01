21eb5b0e11ee080b7224687a1a3151bc217ab74e
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2023-03-01 12:13:46 -0500

    New test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+
 for file in tst/*.s; do
   echo "Running test $file"
   exe=${file%.s}
   $exe | diff --color -y - $exe.out.txt
+  if [ $? -ne 0 ]; then
+    echo "Test $file failed"
+    exit 1
+  fi
 done
